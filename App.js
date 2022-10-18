0a3e9bffc8d5bf2876e9ef31b1173cbca8989a89
Mykhailo Mashchenko <mykhailo.mashchenko@yalantis.net>
2022-10-18 12:18:40 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,421 @@
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+
+import AnimatedKeyboardExample from './src/AnimatedKeyboardExample';
+import AnimatedSensorExample from './src/AnimatedSensorExample';
+import FrameCallbackExample from './src/FrameCallbackExample';
+import AnimatedTextInputExample from './src/AnimatedTextInputExample';
+import AnimatedTextWidthExample from './src/AnimatedTextWidthExample';
+import BokehExample from './src/BokehExample';
+import BouncingBoxExample from './src/BouncingBoxExample';
+import BubblesExample from './src/BubblesExample';
+import ChessboardExample from './src/ChessboardExample';
+import ColorExample from './src/ColorExample';
+import EmptyExample from './src/EmptyExample';
+import GestureHandlerExample from './src/GestureHandlerExample';
+// import MeasureExample from './src/MeasureExample';
+// import NewestShadowNodesRegistryRemoveExample from './src/NewestShadowNodesRegistryRemoveExample';
+// import React from 'react';
+import {GestureHandlerRootView, RectButton} from 'react-native-gesture-handler';
+// import RefExample from './src/RefExample';
+import ScreenStackExample from './src/ScreenStackExample';
+import ScreenStackHeaderConfigBackgroundColorExample from './src/ScreenStackHeaderConfigBackgroundColorExample';
+// import ScrollToExample from './src/ScrollToExample'; // ---
+import ScrollViewExample from './src/ScrollViewExample';
+// // import SvgExample from './src/SvgExample';
+// import TransformExample from './src/TransformExample';
+// import WidthExample from './src/WidthExample';
+// // import WorkletExample from './src/WorkletExample';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ScrollViewOffsetExample from './src/ScrollViewOffsetExample';
+import ArticleProgressExample from './src/ArticleProgressExample';
+
+const EXAMPLES = [
+  {
+    name: 'AnimatedKeyboardExample',
+    icon: '⌨️',
+    title: 'Animated keyboard example',
+    component: AnimatedKeyboardExample,
+  },
+  {
+    name: 'AnimatedTextInputExample',
+    icon: '🎰',
+    title: 'Animated.TextInput value',
+    component: AnimatedTextInputExample,
+  },
+  {
+    name: 'AnimatedTextWidthExample',
+    icon: '✂️',
+    title: 'Animated.Text width',
+    component: AnimatedTextWidthExample,
+  },
+  {
+    name: 'BokehExample',
+    icon: '✨',
+    title: 'Bokeh',
+    component: BokehExample,
+  },
+  {
+    name: 'BubblesExample',
+    icon: '🫧',
+    title: 'Bubbles',
+    component: BubblesExample,
+  },
+  {
+    name: 'ColorExample',
+    icon: '🌈',
+    title: 'Colors',
+    component: ColorExample,
+  },
+  {
+    name: 'ScreenStackHeaderConfigBackgroundColorExample',
+    icon: '🎨',
+    title: 'Screen header background color',
+    component: ScreenStackHeaderConfigBackgroundColorExample,
+  },
+  {
+    name: 'ScreenStackExample',
+    icon: '🥞',
+    title: 'Screen stack',
+    component: ScreenStackExample,
+  },
+  {
+    name: 'GestureHandlerExample',
+    icon: '👌',
+    title: 'Draggable circle',
+    component: GestureHandlerExample,
+  },
+  // // {
+  // //   name: 'SvgExample',
+  // //   icon: '🟢',
+  // //   title: 'Animated SVG circle',
+  // //   component: SvgExample,
+  // // },
+  {
+    name: 'BouncingBoxExample',
+    icon: '📦',
+    title: 'Bouncing box',
+    component: BouncingBoxExample,
+  },
+  {
+    name: 'AnimatedSensorExample',
+    icon: '📡',
+    title: 'useAnimatedSensor',
+    component: AnimatedSensorExample,
+  },
+  {
+    name: 'FrameCallbackExample',
+    icon: '🗣',
+    title: 'useFrameCallback',
+    component: FrameCallbackExample,
+  },
+  {
+    name: 'ScrollViewExample',
+    icon: '📜',
+    title: 'useAnimatedScrollHandler',
+    component: ScrollViewExample,
+  },
+  // {
+  //   name: 'ScrollToExample',
+  //   icon: '🦘',
+  //   title: 'scrollTo',
+  //   component: ScrollToExample,
+  // },
+  {
+    name: 'ScrollViewPositionExample',
+    icon: '𝌍',
+    title: 'useScrollViewPosition',
+    component: ScrollViewOffsetExample,
+  },
+  {
+    name: 'ArticleProgressExample',
+    icon: '📰',
+    title: 'Article Progress Example',
+    component: ArticleProgressExample,
+  },
+
+  // // {
+  // //   name: 'MeasureExample',
+  // //   icon: '📐',
+  // //   title: 'measure',
+  // //   component: MeasureExample,
+  // // },
+  // // {
+  // //   name: 'WorkletExample',
+  // //   icon: '🧵',
+  // //   title: 'runOnJS / runOnUI',
+  // //   component: WorkletExample,
+  // // },
+  // {
+  //   name: 'TransformExample',
+  //   icon: '🔄',
+  //   title: 'Transform',
+  //   component: TransformExample,
+  // },
+  // {
+  //   name: 'WidthExample',
+  //   icon: '🌲',
+  //   title: 'Layout props',
+  //   component: WidthExample,
+  // },
+  // {
+  //   name: 'RefExample',
+  //   icon: '🦑',
+  //   title: 'forwardRef & useImperativeHandle',
+  //   component: RefExample,
+  // },
+  {
+    name: 'ChessboardExample',
+    icon: '♟️',
+    title: 'Chessboard',
+    component: ChessboardExample,
+  },
+  // {
+  //   name: 'NewestShadowNodesRegistryRemoveExample',
+  //   icon: '🌓',
+  //   title: 'Conditional',
+  //   component: NewestShadowNodesRegistryRemoveExample,
+  // },
+  {
+    name: 'EmptyExample',
+    icon: '👻',
+    title: 'Empty',
+    component: EmptyExample,
+  },
+  {
+    name: 'Youtube',
+    icon: '📰',
+    title: 'Youtube',
+    component: YoutubeNavigator,
+  },
+];
+
+console.log('HomeScreen');
+function HomeScreen() {
+  const navigation = useNavigation();
+  console.log('HomeScreen');
+  return (
+    <FlatList
+      style={styles.list}
+      data={EXAMPLES}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({item}) => (
+        <Item
+          title={item.icon + '  ' + item.title}
+          onPress={() => navigation.navigate(item.name)}
+        />
+      )}
+      renderScrollComponent={props => <ScrollView {...props} />}
+    />
+  );
+}
+
+function Item({title, onPress}) {
+  return (
+    <RectButton style={styles.button} onPress={onPress}>
+      <Text style={styles.title}>{title}</Text>
+    </RectButton>
+  );
+}
+
+function ItemSeparator() {
+  return <View style={styles.separator} />;
+}
+
+const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <GestureHandlerRootView style={styles.container}>
+//       <NavigationContainer>
+//         <Stack.Navigator>
+//           <Stack.Screen
+//             name="Home"
+//             component={HomeScreen}
+//             options={{headerTitle: 'Reanimated & Fabric examples'}}
+//           />
+//           {EXAMPLES.map(({name, title, component}) => (
+//             <Stack.Screen
+//               key={name}
+//               name={name}
+//               component={component}
+//               options={{headerTitle: title}}
+//             />
+//           ))}
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </GestureHandlerRootView>
+//   );
+// }
+
+// export const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   list: {
+//     backgroundColor: '#EFEFF4',
+//   },
+//   separator: {
+//     height: 1,
+//     backgroundColor: '#DBDBE0',
+//   },
+//   button: {
+//     flex: 1,
+//     height: 60,
+//     padding: 15,
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     backgroundColor: 'white',
+//   },
+//   title: {
+//     fontSize: 16,
+//     backgroundColor: 'transparent',
+//   },
+// });
+
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  // ScrollView,
+  StatusBar,
+  // StyleSheet,
+  // Text,
+  useColorScheme,
+  // View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import YoutubeNavigator from './src/youtube/YoutubeNavigator';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+// const App: () => Node = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar
+//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+//         backgroundColor={backgroundStyle.backgroundColor}
+//       />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         style={backgroundStyle}>
+//         <Header />
+//         <View
+//           style={{
+//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//           }}>
+//           <Section title="Step One">
+//             Edit <Text style={styles.highlight}>App.js</Text> to change this
+//             screen and then come back to see your edits.
+//           </Section>
+//           <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks />
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
+
+const App = () => {
+  console.log('App');
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{headerTitle: 'Reanimated & Fabric examples'}}
+          />
+          {EXAMPLES.map(({name, title, component}) => (
+            <Stack.Screen
+              key={name}
+              name={name}
+              component={component}
+              options={{headerTitle: title}}
+            />
+          ))}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  container: {
+    flex: 1,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
